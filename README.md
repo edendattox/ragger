3f714fcb7510bccd97db615a20427662ea002765
edendattox <edendattox@gmail.com>
2024-02-05 00:31:36 +0530

    feat: llm call

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ragger
+
+### What is ragger?
+
+ragger is a simple RAG system built in go.
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/edendattox/ragger/cmd"
+
+func main() {
+	cmd.Execute()
+}
